de10e2734e3d0415c8311f8d10408bc7593972b2
Eughami <imamosi50@gmail.com>
2019-04-21 17:02:31 +0300

    Added template with interconnections, added controller and views

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,3 @@
 @extends('layouts.footer')
 @extends('layouts.index')
-@extends('layouts.header')
+@extends('layouts.head')
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,9 +6,10 @@
 
 	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
 
-	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
-	<link rel="stylesheet" href="css/style.css"> <!-- Gem style -->
-	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+	<link rel="stylesheet" href="{{asset('css/reset.css')}}"> <!-- CSS reset -->
+	<link rel="stylesheet" href="{{asset('css/style.css')}}"> <!-- Gem style -->
+	<script src="{{asset('js/modernizr.js')}}"></script> <!-- Modernizr -->
   	
 	<title>Адаптивные модальные формы входа и регистрации</title>
 </head>
+
