b4cfe73918c1e3536bb5927691a6dd84115fd550
Manucheckhr Rakhimov <manucher5160@gmail.com>
2019-04-21 17:18:03 +0300

    Created Migration

---
+++ database/migrations/2019_04_21_141217_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->integer('yearOfBirth');
+            $table->string('sex');
+            $table->string('address');
+            $table->integer('phoneNumber');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
